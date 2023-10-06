7d5199c7a79a579790ce2c593149bdb1642caf42
Kishan Aher <kishanaher12@gmail.com>
2023-10-06 12:51:12 +0530

    simple couter app is created

---
+++ 02_couter_app/src/App.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-no-undef */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const IncreaseCount= ()=>{
+  setCount(count+1);
+  console.log("clicked",count);
+  }
+
+  //function for decrease counter
+  const DecreaseCout=()=>{
+    (count>0)?setCount(count-1):0;
+    console.log("clicked",count)
+  }
+
+  return (
+    <div>
+      <h1>Chai aur Code </h1>
+      <h2>Counter App : {count}</h2>
+      <br/>
+      <button type="button" onClick={IncreaseCount} style={{margin:'2px'}} >Increase count</button>
+      <button onClick={DecreaseCout}>Decrease count</button>
+    </div>
+  )
+}
+
+export default App
